4fedb4f87704a77760c39d5571e0de4731aaa590
Tim <timvaneert@upcmail.nl>
2020-02-12 14:56:48 +0100

    v.1.1 Date : 12/02/20

---
+++ js/app.js
@@ -0,0 +1,6 @@
+let startBtn = document.querySelector("#startBtn");
+let startPage = document.querySelector(".startP");
+
+startBtn.addEventListener("click", () => {
+  startPage.innerHTML = "";
+});
